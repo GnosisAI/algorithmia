3f1fbf7c08dabdbc6696b6d3d1c25d493ee47c6e
zakaria fellah <zkrfellah@gmail.com>
2018-09-11 17:01:18 -0700

    style

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,36 +1,31 @@
 <template>
-          <div>
-        <div id="site-banner" data-v-03528ac7>
-          <!---->
-        </div>
-        <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-          <div id="site-banner" data-v-03528ac7>
-            <!---->
-          </div>
-          <div class="container-fluid navbar-container mh-16">
-            <div class="navbar-header"><button type="button" data-toggle="collapse" data-target="#primary-navbar-collapse"
-                aria-expanded="false" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button> <a
-                href="/" title="Algorithmia - Home" class="navbar-brand active"><span id="nav-logo" class="nav-logo-full"></span></a>
-              <!---->
+   <div>
+      <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+         <div class="container-fluid navbar-container mh-16">
+            <div class="navbar-header">
+               <button type="button" data-toggle="collapse" data-target="#primary-navbar-collapse"
+                  aria-expanded="false" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+               </button> 
+               <a href="/" title="Zakaria CV" class="navbar-brand active"><span id="nav-logo" class="nav-logo-full"></span>
+                  Zakaria CV
+                </a>
+               <!---->
             </div>
             <div id="primary-navbar-collapse" class="collapse navbar-collapse">
-              <div>
-                <ul class="nav navbar-nav navbar-links">
-                  <li class="visible-lg visible-md"><a><span>For Engineering</span></a></li>
-                  <li class="visible-lg visible-md"><a><span>For Data Science</span></a></li>
-                  <li class="visible-lg visible-md"><a><span>For Enterprise</span></a></li>
-                  <li class="hidden-md hidden-lg active"><a href="/algorithms" class="router-link-exact-active active"><span>AI
-                        Marketplace</span></a></li>
-                </ul>
-                <!---->
-              </div>
+               <div>
+                  <ul class="nav navbar-nav navbar-links">
+                     <li class="visible-lg visible-md"><a href="/"><span>Home</span></a></li>
+                     <li class="visible-lg visible-md"><a><span>Algorithms</span></a></li>
+                     <li class="visible-lg visible-md"><a href="www.zakaria.im"><span>Contact</span></a></li>
+                  </ul>
+                  <!---->
+               </div>
             </div>
-          </div>
-        </nav>
-      </div>
+         </div>
+      </nav>
+   </div>
 </template>
-
 <script>
 export default {
   name: 'Nav',
--- src/components/home/Search.vue
+++ src/components/home/Search.vue
@@ -1,19 +1,30 @@
 <template>
         <div class="col-md-12 mb-32" >
-            <div class="header-search" >
-              <form id="header-search-form">
-                <div class="input-group"><input name="q" type="search" placeholder="Search..." autocomplete="off" value=""
+                <div class="input-group"><input  v-model="value" placeholder="Search..." autocomplete="off" 
                     class="form-control input-lg">
-                  <div class="input-group-btn"><button type="submit" class="btn btn-default btn-lg"><i aria-hidden="true"
-                        class="fa fa-search"></i></button></div>
+                  <div class="input-group-btn">
+                      <button type="submit" @click="searchAlgo()"  class="btn btn-default btn-lg">
+                      <i aria-hidden="true" class="fa fa-search"></i>
+                      </button>
+                  </div>
                 </div>
-              </form>
-            </div>
+            <hr>
           </div>
 </template>
 <script>
 export default {
   name: 'Search',
+  data(){
+    return {
+      value: this.route.query.q
+    }
+  },
+  methods:{
+    searchAlgo(){
+       this.$router.push({ path: '/search/?q='+this.value})
+    }
+  }
+
 
 }
 </script>
